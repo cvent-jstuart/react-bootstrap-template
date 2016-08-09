3c568f3d075a678863bda1d737a0329bf2e7ebab
Stuart, Jason <jstuart@cvent.com>
2016-08-09 18:35:26 -0400

    CONVERT-TABLE-PAGE: Converted Table.js

--- src/assets/js/routes/Bootstrap/Table/Table.js
+++ src/assets/js/routes/Bootstrap/Table/Table.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-import Table from "../../../components/Bootstrap/Table/Table";
+import { Table, PageHeader, Grid, Row, Col } from "react-bootstrap";
+import PropTable from "../../../components/PropTable/PropTable";
+import ComponentHeader from "../../../components/ComponentHeader/ComponentHeader";
+import ComponentExample from "../../../components/ComponentExample/ComponentExample";
 
 // require("./.less");
 
@@ -9,11 +12,248 @@ export default class TablesRoute extends React.Component {
     return (
       <div className="body">
 
-        <div class="page-header">
-          <h1>Tables</h1>
-        </div>
-        <Table />
+        <PageHeader>Tables</PageHeader>
 
+        <Grid>
+          <Row>
+            <Col>
+
+              <ComponentHeader
+                subtitle="Use the striped, bordered, condensed and hover props to customise the table."
+              />
+
+              <ComponentExample>
+                <Table striped bordered condensed hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td colSpan="2">Larry the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </ComponentExample>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr></hr>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+
+              <ComponentHeader
+                title="Responsive"
+                subtitle="Add responsive prop to make them scroll horizontally up to small devices (under 768px). When viewing on anything larger than 768px wide, you will not see any difference in these tables."
+              />
+
+              <ComponentExample>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Table heading</th>
+                      <th>Table heading</th>
+                      <th>Table heading</th>
+                      <th>Table heading</th>
+                      <th>Table heading</th>
+                      <th>Table heading</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                      <td>Table cell</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </ComponentExample>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr></hr>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+
+              <PropTable title="Table Props" >
+                <Table striped bordered>
+                  <thead>
+                		<tr>
+                			<th>
+                				Name
+                			</th>
+                			<th>
+                				Type
+                			</th>
+                			<th>
+                				Default
+                			</th>
+                			<th>
+                				Description
+                			</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<tr>
+                			<td>
+                				bordered
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				bsClass
+                			</td>
+                			<td>
+                				<div>
+                					string
+                				</div>
+                			</td>
+                			<td>
+                				'table'
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						Base CSS class and prefix for the component. Generally one should only change
+                						<code>
+                							bsClass
+                						</code>
+                						 to provide new, non-Bootstrap, CSS styles for a component.
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				condensed
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				hover
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				responsive
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				striped
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                	</tbody>
+                </Table>
+              </PropTable>
+
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
