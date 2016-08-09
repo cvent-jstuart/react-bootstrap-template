305085350915a081723b9a3847a53f10baee651c
Stuart, Jason <jstuart@cvent.com>
2016-08-09 17:10:19 -0400

    RETRO-PAGEHEADER-GRID: Converted Breadcrumb.js

--- src/assets/js/routes/Bootstrap/Breadcrumb/Breadcrumb.js
+++ src/assets/js/routes/Bootstrap/Breadcrumb/Breadcrumb.js
@@ -1,180 +1,189 @@
 import React from "react";
 
-import { Breadcrumb, Table } from "react-bootstrap";
+import { Breadcrumb, Table, PageHeader, Grid, Row, Col } from "react-bootstrap";
 import PropTable from "../../../components/PropTable/PropTable";
 import ComponentHeader from "../../../components/ComponentHeader/ComponentHeader";
 import ComponentExample from "../../../components/ComponentExample/ComponentExample";
 
 // require("./.less");
 
-export default class NavBarsRoute extends React.Component {
+export default class BreadcrumbsRoute extends React.Component {
   render() {
     return (
       <div className="body">
 
-        <div class="page-header">
-          <h3>Breadcrumbs</h3>
-        </div>
-        <div>
+        <PageHeader>Breadcrumbs</PageHeader>
 
-          <ComponentHeader
-            title="Breadcrumbs Example"
-            subtitle={["Breadcrumbs are used to indicate the current page's location. Add active attribute to active Breadcrumb.Item.","Do not set both active and href attributes. active overrides href and span element is rendered instead of a."]}
-          />
+        <Grid>
+          <Row>
+            <Col>
 
-          <ComponentExample>
-            <Breadcrumb>
-              <Breadcrumb.Item href="#">
-                Home
-              </Breadcrumb.Item>
-              <Breadcrumb.Item href="http://getbootstrap.com/components/#breadcrumbs">
-                Library
-              </Breadcrumb.Item>
-              <Breadcrumb.Item active>
-                Data
-              </Breadcrumb.Item>
-            </Breadcrumb>
-          </ComponentExample>
+              <ComponentHeader
+                title="Breadcrumbs Example"
+                subtitle={["Breadcrumbs are used to indicate the current page's location. Add active attribute to active Breadcrumb.Item.","Do not set both active and href attributes. active overrides href and span element is rendered instead of a."]}
+              />
 
-        </div>
-        <hr></hr>
-        <div>
+              <ComponentExample>
+                <Breadcrumb>
+                  <Breadcrumb.Item href="#">
+                    Home
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item href="http://getbootstrap.com/components/#breadcrumbs">
+                    Library
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item active>
+                    Data
+                  </Breadcrumb.Item>
+                </Breadcrumb>
+              </ComponentExample>
 
-          <PropTable
-            title="Breadcrumb.Item props"
-            subtitle="Breadcrumb component itself doesn't have any specific public properties."
-          >
-            <Table striped bordered>
-              <thead>
-            		<tr>
-            			<th>
-            				Name
-            			</th>
-            			<th>
-            				Type
-            			</th>
-            			<th>
-            				Default
-            			</th>
-            			<th>
-            				Description
-            			</th>
-            		</tr>
-            	</thead>
-            	<tbody>
-            		<tr>
-            			<td>
-            				active
-            			</td>
-            			<td>
-            				<div>
-            					boolean
-            				</div>
-            			</td>
-            			<td>
-            				false
-            			</td>
-            			<td>
-            				<div>
-            					<p>
-            						If set to true, renders
-            						<code>
-            							span
-            						</code>
-            						 instead of
-            						<code>
-            							a
-            						</code>
-            					</p>
-            				</div>
-            			</td>
-            		</tr>
-            		<tr>
-            			<td>
-            				href
-            			</td>
-            			<td>
-            				<div>
-            					string
-            				</div>
-            			</td>
-            			<td>
-            				&nbsp;
-            			</td>
-            			<td>
-            				<div>
-            					<p>
-            						<code>
-            							href
-            						</code>
-            						 attribute for the inner
-            						<code>
-            							a
-            						</code>
-            						 element
-            					</p>
-            				</div>
-            			</td>
-            		</tr>
-            		<tr>
-            			<td>
-            				target
-            			</td>
-            			<td>
-            				<div>
-            					string
-            				</div>
-            			</td>
-            			<td>
-            				&nbsp;
-            			</td>
-            			<td>
-            				<div>
-            					<p>
-            						<code>
-            							target
-            						</code>
-            						 attribute for the inner
-            						<code>
-            							a
-            						</code>
-            						 element
-            					</p>
-            				</div>
-            			</td>
-            		</tr>
-            		<tr>
-            			<td>
-            				title
-            			</td>
-            			<td>
-            				<div>
-            					node
-            				</div>
-            			</td>
-            			<td>
-            				&nbsp;
-            			</td>
-            			<td>
-            				<div>
-            					<p>
-            						<code>
-            							title
-            						</code>
-            						 attribute for the inner
-            						<code>
-            							a
-            						</code>
-            						 element
-            					</p>
-            				</div>
-            			</td>
-            		</tr>
-            	</tbody>
-            </Table>
-          </PropTable>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr></hr>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
 
-        </div>
+              <PropTable
+                title="Breadcrumb.Item props"
+                subtitle="Breadcrumb component itself doesn't have any specific public properties."
+              >
+                <Table striped bordered>
+                  <thead>
+                		<tr>
+                			<th>
+                				Name
+                			</th>
+                			<th>
+                				Type
+                			</th>
+                			<th>
+                				Default
+                			</th>
+                			<th>
+                				Description
+                			</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<tr>
+                			<td>
+                				active
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						If set to true, renders
+                						<code>
+                							span
+                						</code>
+                						 instead of
+                						<code>
+                							a
+                						</code>
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				href
+                			</td>
+                			<td>
+                				<div>
+                					string
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						<code>
+                							href
+                						</code>
+                						 attribute for the inner
+                						<code>
+                							a
+                						</code>
+                						 element
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				target
+                			</td>
+                			<td>
+                				<div>
+                					string
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						<code>
+                							target
+                						</code>
+                						 attribute for the inner
+                						<code>
+                							a
+                						</code>
+                						 element
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				title
+                			</td>
+                			<td>
+                				<div>
+                					node
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						<code>
+                							title
+                						</code>
+                						 attribute for the inner
+                						<code>
+                							a
+                						</code>
+                						 element
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                	</tbody>
+                </Table>
+              </PropTable>
+
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
