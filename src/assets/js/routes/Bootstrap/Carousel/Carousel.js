efdd186b8195d20b325a27bb29e9bb0a86daaa95
Stuart, Jason <jstuart@cvent.com>
2016-08-10 18:25:30 -0400

    CREATE-MEDIAOBJECTS-PAGE: Reset Carousel.js code to original.

--- src/assets/js/routes/Bootstrap/Carousel/Carousel.js
+++ src/assets/js/routes/Bootstrap/Carousel/Carousel.js
@@ -1,44 +1,73 @@
 import React from "react";
 
-import { Thumbnail, Button, Table, PageHeader, Grid, Row, Col } from "react-bootstrap";
+import { Carousel,Table, PageHeader, Grid, Row, Col } from "react-bootstrap";
 import PropTable from "../../../components/PropTable/PropTable";
 import ComponentHeader from "../../../components/ComponentHeader/ComponentHeader";
 import ComponentExample from "../../../components/ComponentExample/ComponentExample";
 
 // require("./.less");
 
-export default class CarouselsRoute extends React.Component {
+export default class ThumbnailsRoute extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0,
+      direction: null
+    }
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(selectedIndex, e) {
+    alert('selected=' + selectedIndex + ', direction=' + e.direction);
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+
   render() {
-    const imgsrc = "https://pbs.twimg.com/profile_images/615993481685794816/vMhSLk-2.jpg";
+    const img1src = "http://www.auspaws.com.au/wp-content/uploads/2015/08/iStock_000060885966_Large-16x9-small.jpg";
+    const img2src = "http://images.nickjr.com/nickjr/promos/video/nickjr-originals/nickjr-puppies/nick-jr-puppies-doggie-beach-16x9.jpg?quality=0.60";
+    const img3src = "http://i.onionstatic.com/onion/2515/6/16x9/960.jpg";
 
     return (
       <div className="body">
 
-        <PageHeader>Thumbnails</PageHeader>
+        <PageHeader>Carousels <small>Carousel, Carousel.Item, Carousel.Caption</small></PageHeader>
 
         <Grid>
           <Row>
             <Col>
 
               <ComponentHeader
-                title="Anchor Thumbnail"
-                subtitle="Creates an anchor wrapping an image."
+                title="Uncontrolled"
+                subtitle="Allow the component to control its own state."
               />
 
               <ComponentExample>
-                <Grid>
-                  <Row>
-                    <Col xs={6} md={3}>
-                      <Thumbnail href="#" alt="171x180" src={imgsrc} />
-                    </Col>
-                    <Col xs={6} md={3}>
-                      <Thumbnail href="#" alt="171x180" src={imgsrc} />
-                    </Col>
-                    <Col xs={6} md={3}>
-                      <Thumbnail href="#" alt="171x180" src={imgsrc} />
-                    </Col>
-                  </Row>
-                </Grid>
+                <Carousel>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img1src} />
+                    <Carousel.Caption>
+                      <h3>First slide label</h3>
+                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img2src} />
+                    <Carousel.Caption>
+                      <h3>Second slide label</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img3src} />
+                    <Carousel.Caption>
+                      <h3>Third slide label</h3>
+                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
               </ComponentExample>
 
             </Col>
@@ -52,45 +81,34 @@ export default class CarouselsRoute extends React.Component {
             <Col>
 
               <ComponentHeader
-                title="Divider Thumbnail"
-                subtitle="Creates a divider wrapping an image and other children elements."
+                title="Controlled"
+                subtitle="Pass down the active state on render via props."
               />
 
               <ComponentExample>
-                <Grid>
-                  <Row>
-                    <Col xs={6} md={4}>
-                      <Thumbnail src={imgsrc} alt="242x200">
-                        <h3>Thumbnail label</h3>
-                        <p>Description</p>
-                        <p>
-                          <Button bsStyle="primary">Button</Button>&nbsp;
-                          <Button bsStyle="default">Button</Button>
-                        </p>
-                      </Thumbnail>
-                    </Col>
-                    <Col xs={6} md={4}>
-                      <Thumbnail src={imgsrc} alt="242x200">
-                        <h3>Thumbnail label</h3>
-                        <p>Description</p>
-                        <p>
-                          <Button bsStyle="primary">Button</Button>&nbsp;
-                          <Button bsStyle="default">Button</Button>
-                        </p>
-                      </Thumbnail>
-                    </Col>
-                    <Col xs={6} md={4}>
-                      <Thumbnail src={imgsrc} alt="242x200">
-                        <h3>Thumbnail label</h3>
-                        <p>Description</p>
-                        <p>
-                          <Button bsStyle="primary">Button</Button>&nbsp;
-                          <Button bsStyle="default">Button</Button>
-                        </p>
-                      </Thumbnail>
-                    </Col>
-                  </Row>
-                </Grid>
+                <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img1src} />
+                    <Carousel.Caption>
+                      <h3>First slide label</h3>
+                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img2src} />
+                    <Carousel.Caption>
+                      <h3>Second slide label</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src={img3src} />
+                    <Carousel.Caption>
+                      <h3>Third slide label</h3>
+                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
               </ComponentExample>
 
             </Col>
@@ -104,7 +122,7 @@ export default class CarouselsRoute extends React.Component {
             <Col>
 
               <PropTable
-                title="Thumbnail Props"
+                title="Crousel Props"
               >
                 <Table striped bordered>
                   <thead>
@@ -126,11 +144,11 @@ export default class CarouselsRoute extends React.Component {
                 	<tbody>
                 		<tr>
                 			<td>
-                				alt
+                				activeIndex
                 			</td>
                 			<td>
                 				<div>
-                					string
+                					number
                 				</div>
                 			</td>
                 			<td>
@@ -150,7 +168,7 @@ export default class CarouselsRoute extends React.Component {
                 				</div>
                 			</td>
                 			<td>
-                				'thumbnail'
+                				'carousel'
                 			</td>
                 			<td>
                 				<div>
@@ -166,11 +184,27 @@ export default class CarouselsRoute extends React.Component {
                 		</tr>
                 		<tr>
                 			<td>
-                				href
+                				controls
                 			</td>
                 			<td>
                 				<div>
-                					string
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				true
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				defaultActiveIndex
+                			</td>
+                			<td>
+                				<div>
+                					number
                 				</div>
                 			</td>
                 			<td>
@@ -182,11 +216,316 @@ export default class CarouselsRoute extends React.Component {
                 		</tr>
                 		<tr>
                 			<td>
-                				src
+                				direction
                 			</td>
                 			<td>
                 				<div>
-                					string
+                					one of:
+                					<code>
+                						'prev'
+                					</code>
+                					,
+                					<code>
+                						'next'
+                					</code>
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				indicators
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				true
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				interval
+                			</td>
+                			<td>
+                				<div>
+                					number
+                				</div>
+                			</td>
+                			<td>
+                				5000
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				nextIcon
+                			</td>
+                			<td>
+                				<div>
+                					node
+                				</div>
+                			</td>
+                			<td>
+                				&lt;Glyphicon glyph="chevron-right" /&gt;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				onSelect
+                			</td>
+                			<td>
+                				<div>
+                					function
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						Callback fired when the active item changes.
+                					</p>
+                					<pre>
+                						<code>
+                							(eventKey: any) =&gt; any | (eventKey: any, event: Object) =&gt; any
+                						</code>
+                					</pre>
+                					<p>
+                						If this callback takes two or more arguments, the second argument will be a persisted event object with
+                						<code>
+                							direction
+                						</code>
+                						 set to the direction of the transition.
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				onSlideEnd
+                			</td>
+                			<td>
+                				<div>
+                					function
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				pauseOnHover
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				true
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				prevIcon
+                			</td>
+                			<td>
+                				<div>
+                					node
+                				</div>
+                			</td>
+                			<td>
+                				&lt;Glyphicon glyph="chevron-left" /&gt;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				slide
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				true
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				wrap
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				true
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                	</tbody>
+                </Table>
+              </PropTable>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr></hr>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+
+              <PropTable
+                title="Carousel.Item Props"
+              >
+                <Table striped bordered>
+                  <thead>
+                		<tr>
+                			<th>
+                				Name
+                			</th>
+                			<th>
+                				Type
+                			</th>
+                			<th>
+                				Default
+                			</th>
+                			<th>
+                				Description
+                			</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<tr>
+                			<td>
+                				active
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				animateIn
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				animateOut
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				direction
+                			</td>
+                			<td>
+                				<div>
+                					one of:
+                					<code>
+                						'prev'
+                					</code>
+                					,
+                					<code>
+                						'next'
+                					</code>
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				index
+                			</td>
+                			<td>
+                				<div>
+                					number
+                				</div>
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				onAnimateOutEnd
+                			</td>
+                			<td>
+                				<div>
+                					function
                 				</div>
                 			</td>
                 			<td>
