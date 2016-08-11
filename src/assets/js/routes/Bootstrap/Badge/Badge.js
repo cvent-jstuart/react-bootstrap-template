6fc3ef20aa232b38fe58d7e6820b267c74c042c6
Stuart, Jason <jstuart@cvent.com>
2016-08-11 10:13:51 -0400

    CREATE-MISC-COMPONENT-PAGE: Converted Badge.js

--- src/assets/js/routes/Bootstrap/Badge/Badge.js
+++ src/assets/js/routes/Bootstrap/Badge/Badge.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-import Badge from "../../../components/Bootstrap/Badge/Badge";
+import { Badge, Table, PageHeader, Grid, Row, Col } from "react-bootstrap";
+import PropTable from "../../../components/PropTable/PropTable";
+import ComponentHeader from "../../../components/ComponentHeader/ComponentHeader";
+import ComponentExample from "../../../components/ComponentExample/ComponentExample";
 
 // require("./.less");
 
@@ -9,11 +12,98 @@ export default class BadgesRoute extends React.Component {
     return (
       <div className="body">
 
-        <div class="page-header">
-          <h1>Badges</h1>
-        </div>
-        <Badge />
+        <PageHeader>Badges</PageHeader>
 
+        <Grid>
+          <Row>
+            <Col>
+
+              <ComponentHeader
+                subtitle="Easily highlight new or unread items by adding a <Badge> to links, Bootstrap navs, and more."
+              />
+
+              <ComponentExample>
+                <p>Badges <Badge>42</Badge></p>
+              </ComponentExample>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr></hr>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+
+              <PropTable
+                title="Badge Props"
+              >
+                <Table striped bordered>
+                  <thead>
+                		<tr>
+                			<th>
+                				Name
+                			</th>
+                			<th>
+                				Type
+                			</th>
+                			<th>
+                				Default
+                			</th>
+                			<th>
+                				Description
+                			</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<tr>
+                			<td>
+                				bsClass
+                			</td>
+                			<td>
+                				<div>
+                					string
+                				</div>
+                			</td>
+                			<td>
+                				'badge'
+                			</td>
+                			<td>
+                				<div>
+                					<p>
+                						Base CSS class and prefix for the component. Generally one should only change
+                						<code>
+                							bsClass
+                						</code>
+                						 to provide new, non-Bootstrap, CSS styles for a component.
+                					</p>
+                				</div>
+                			</td>
+                		</tr>
+                		<tr>
+                			<td>
+                				pullRight
+                			</td>
+                			<td>
+                				<div>
+                					boolean
+                				</div>
+                			</td>
+                			<td>
+                				false
+                			</td>
+                			<td>
+                				&nbsp;
+                			</td>
+                		</tr>
+                	</tbody>
+                </Table>
+              </PropTable>
+
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
