a7b70113008869ba9d44442081fa071140c9a10e
Stuart, Jason <jstuart@cvent.com>
2016-08-11 11:12:54 -0400

    CREATE-MISC-COMPONENT-PAGE: Edited Closeable Alerts in Alert.js

--- src/assets/js/routes/Bootstrap/Alert/Alert.js
+++ src/assets/js/routes/Bootstrap/Alert/Alert.js
@@ -71,17 +71,19 @@ export default class AlertsRoute extends React.Component {
                 subtitle="Just pass in an onDismiss function."
               />
 
-              {this.state.alertVisible ?
-                 <Alert bsStyle="danger" onDismiss={this.handleAlertDismiss}>
-                   <h4>Oh snap! You got an error!</h4>
-                   <p>Change this and that and try again. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
-                   <p>
-                     <Button bsStyle="danger">Take this action</Button>
-                     <span> or </span>
-                     <Button onClick={this.handleAlertDismiss}>Hide Alert</Button>
-                   </p>
-                 </Alert>
-               : null }
+            <ComponentExample>
+                {this.state.alertVisible ?
+                   <Alert bsStyle="danger" onDismiss={this.handleAlertDismiss}>
+                     <h4>Oh snap! You got an error!</h4>
+                     <p>Change this and that and try again. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                     <p>
+                       <Button bsStyle="danger">Take this action</Button>
+                       <span> or </span>
+                       <Button onClick={this.handleAlertDismiss}>Hide Alert</Button>
+                     </p>
+                   </Alert>
+                 : null }
+               </ComponentExample>
 
             </Col>
           </Row>
