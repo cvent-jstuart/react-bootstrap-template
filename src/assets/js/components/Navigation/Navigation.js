878dc8f80ace66b966a43156c5c3147a7f5d0bc4
Stuart, Jason <jstuart@cvent.com>
2016-08-12 12:55:05 -0400

    CONVERT-MAIN-NAVIGATION: Converted Navigation.js to react-bootstrap. Modified Layout background styles.

--- src/assets/js/components/Navigation/Navigation.js
+++ src/assets/js/components/Navigation/Navigation.js
@@ -1,77 +1,75 @@
 import React from "react";
-import { Link } from "react-router";
 
-import NavigationBranding from "./NavigationBranding/NavigationBranding";
-import NavBar from "./NavBar/NavBar";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const links = [
   "Home",
   {
     "Buttons": [
       "Buttons",
-      "ButtonGroups",
+      "Button Groups",
       "Dropdowns"
     ]
   },
   {
     "Overlays": [
       "Modals",
-      "Tooltips",
-      "Popovers"
+      "Popovers",
+      "Tooltips"
+
     ]
   },
   {
     "Navigation": [
+      "Breadcrumbs",
       "Navs",
       "Navbars",
-      "Breadcrumbs",
-      "Tabs",
+      "Pagers",
       "Paginations",
-      "Pagers"
+      "Tabs"
     ]
   },
   {
     "Page Layout": [
       "Grids",
       "Jumbotrons",
-      "PageHeaders",
-      "ListGroups",
-      "Tables",
+      "List Groups",
+      "Page Headers",
       "Panels",
+      "Tables",
       "Wells"
     ]
   },
   {
     "Forms": [
       "Forms",
-      "SupportedControls",
-      "FormLayouts",
-      "InputGroups",
-      "ValidationStates"
+      "Form Layouts",
+      "Input Groups",
+      "Supported Controls",
+      "Validation States"
     ]
   },
   {
     "Media Content": [
-      "Images",
-      "Thumbnails",
-      "ResponsiveEmbeds",
       "Carousels",
-      "MediaObjects"
+      "Images",
+      "Media Objects",
+      "Responsive Embeds",
+      "Thumbnails"
     ]
   },
   {
     "Miscellaneous": [
+      "Alerts",
+      "Badges",
       "Glyphicons",
       "Labels",
-      "Badges",
-      "Alerts",
-      "Progressbars"
-    ]
-  },
-  {
-    "Utilities": [
-      "Transitions",
-      "CustomStyles"
+      "Progressbars",
+      "Divider",
+      "Header:Utilities",
+      "Custom Styles",
+      "Transitions"
     ]
   }
 ]
@@ -79,17 +77,78 @@ const links = [
 require("./navigation.less");
 
 export default class Navigation extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      "selectedNavKey": null
+    };
+    this.onNavSelect = this.onNavSelect.bind(this);
+  }
+
+  onNavSelect(selectedKey) {
+    this.setState({ selectedNavKey: selectedKey })
+  }
+
   render() {
     return (
-      <div class="navigation-container">
-        {/* Fixed navbar */}
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-          <div class="container">
-            <NavigationBranding title="Bootstrap Template" />
-            <NavBar links={links} /> {/*/.nav-collapse */}
-          </div>
-        </nav>
-      </div>
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/index.html">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav activeKey={ this.state.selectedNavKey } onSelect={ this.onNavSelect } >
+            {
+              links.map((link, i) => {
+                if (typeof link === "object" && link !== null) {
+                  let linkKey = Object.keys(link);
+
+                  return  <NavDropdown
+                            title={ linkKey }
+                            eventKey={ linkKey }
+                            id={ linkKey }
+                          >
+                            {
+                              link[linkKey].map((inner, i) => {
+                                let innerLinkKey = link[linkKey][i];
+                                let menuCode = innerLinkKey.replace(/ /g, "");
+
+                                if (innerLinkKey === "Divider") {
+                                  return  <MenuItem divider />
+                                } else if (innerLinkKey.indexOf("Header:") == 0) {
+                                  return  <MenuItem header >
+                                            { innerLinkKey.split("Header:")[1] }
+                                          </MenuItem>
+                                } else {
+                                  return  <LinkContainer to={menuCode}>
+                                            <MenuItem
+                                              eventKey={ menuCode }
+                                            >
+                                              { innerLinkKey }
+                                            </MenuItem>
+                                          </LinkContainer>
+                                }
+                              })
+                            }
+                          </NavDropdown>
+                } else {
+                  let menuCode = link.replace(/ /g, "");
+
+                  return  <LinkContainer to={menuCode} >
+                            <NavItem
+                              eventKey={ menuCode }
+                            >
+                              { link }
+                            </NavItem>
+                          </LinkContainer>
+                }
+              })
+            }
+          </Nav>
+      </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
